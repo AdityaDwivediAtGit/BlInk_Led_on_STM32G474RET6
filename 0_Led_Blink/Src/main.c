b7b9a05c5d80d94163223bd1e8897134be945f85
AdityaDwivediAtGit <107645490+AdityaDwivediAtGit@users.noreply.github.com>
2022-10-05 01:42:19 +0530

    #5 Defined AHB1 Address from Memory Map

--- 0_Led_Blink/Src/main.c
+++ 0_Led_Blink/Src/main.c
@@ -1,1 +1,13 @@
-// LED is connected to PA5 (Refer User LD2 in User Guide Pg 22)
+/* 
+ * LED is connected to PA5 (Refer User LD2 in User Guide Pg 22)
+ * PA5 is connected to GPIO PORT A (refer Block Diagram in Datasheet Pg 17/236)
+ * GPIO PORT A is getting clock from AHB2 (refer Block Diagram in Datasheet Pg 17/236)
+ * Addresses of Peripherals (refer Memory Map in Reference Manual Page 82/2124)
+ * Enabling Clock for GPIO A (RCC is connected to AHB1)
+
+`
+
+*/
+#define PERIPHERAL_BASE         0x40000000UL
+#define AHB1_PERIPHERAL_OFFSET	0x00020000UL
+#define AHB1_BASE (PERIPHERAL_BASE + AHB1_PERIPHERAL_OFFSET)
